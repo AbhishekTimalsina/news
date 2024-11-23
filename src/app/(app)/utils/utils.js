5abab13cbb22b4565ffac5a627c6a5d929ffe1cd
AbhishekTimalsina <hahaguyz546@gmail.com>
2024-11-23 20:58:20 +0545

    ...

--- src/app/(app)/utils/utils.js
+++ src/app/(app)/utils/utils.js
@@ -1,4 +1,5 @@
 export function FormatDate(unformattedDate) {
+  if (!unformattedDate) unformattedDate = new Date()
   let date = new Date(unformattedDate)
   const options = { day: 'numeric', month: 'long', year: 'numeric' }
   const formattedDate = new Intl.DateTimeFormat('en-US', options).format(date)
